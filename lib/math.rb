8271239b49c92a465e831c4610387538dea3caca
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-28 16:46:40 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  difference
 end
 
 def division(num1, num2)
-
+  answer = num1 / num2
+  answer
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  remainder
 end
 
 def square_root(num)
-
+  p Integer.sqrt(num)
 end
